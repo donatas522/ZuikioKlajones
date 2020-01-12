e520ef649b1b01e4b2acac2ab534bbdbbe23f405
SilkyRock <59670053+SilkyRock@users.noreply.github.com>
2020-01-12 15:10:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -4,14 +4,25 @@ max_steps_per_episode = 100
 
 
 # ----- World Settings -----
-rows = 20 # world rows
-cols = 20 # world columns
+rows = 10 # world rows
+cols = 10 # world columns
 N = rows * cols # number of world cells
+M = 10 # number of apples
+directions = 8 # neigbouring cells
 background_color = '#FFFFFF' # white
 wall_color = '#000000' # black
 rabbit_color = '#0000FF' # blue
 wolf_color = '#FF000' # red
 carrot_color = '#00FF00' # green
+
+
+# ----- Agent Settings -----
+rabbit_directions = 8
+rabbit_actions = 8
+rabbit_lookdist = 4
+wolf_directions = 4
+wolf_actions = 7
+wolf_lookdist = 4
 
 
 # ----- Learning Parameters -----
@@ -23,11 +34,6 @@ epsilon_decay = 0.001 # exploration decay rate
 
 
 # ----- Reward/Punishement -----
-M = 10
-eat_carrot = M
+eat_apple = M
 eaten_by_wolf = N//4
 move = -1
-
-# determine how many directions can agent moves.
-directions = 8   # you may change it to 4: up,down,left and right.
-lookdist = 4
