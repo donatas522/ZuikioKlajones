e17f39453b25b6eb69971a8b06a5ba7a05e32dcc
SilkyRock <59670053+SilkyRock@users.noreply.github.com>
2020-01-14 14:09:43 +0200

    pakeiciau tik kai kuriu kintamuju pavadinimus, nieko daugiau

--- qlearn.py
+++ qlearn.py
@@ -7,13 +7,13 @@ import numpy as np
 class QLearn:
     
     def __init__(self, actions, alpha=cfg.alpha, gamma=cfg.gamma, epsilon=cfg.epsilon, min_epsilon=cfg.min_epsilon, epsilon_decay=cfg.epsilon_decay):
-        self.Q = {}
+        self.Q = {} # {(state, action): Q-value}
         self.alpha = alpha
         self.gamma = gamma
         self.epsilon = epsilon
         self.min_epsilon = min_epsilon
         self.epsilon_decay = epsilon_decay
-        self.actions = actions # a list of actions
+        self.actions = actions # a list of actions: [action0, action1, action2, ...]
     
     # Get the Q-value of an action in a certain state, default value is 0.0
     def getQ(self, state, action):
@@ -25,26 +25,24 @@ class QLearn:
             action = random.choice(self.actions) # choose a random action
         else: # exploitation
             Q = [self.getQ(state, act) for act in self.actions] # Q-values for a specific state for all actions
-            maxQ = max(Q)
+            max_Q = max(Q)
             # In case there are several max Q-values, select one at random
-            if Q.count(maxQ) > 1:
-                bestActions = [self.actions[i] for i in range(len(self.actions)) if Q[i] == maxQ]
-                action = random.choice(bestActions)
+            if Q.count(max_Q) > 1:
+                best_actions = [self.actions[i] for i in range(len(self.actions)) if Q[i] == max_Q]
+                action = random.choice(best_actions)
             else:
-                action = self.actions[Q.index(maxQ)]
+                action = self.actions[Q.index(max_Q)]
         return action
     
     # Learn
     def learnQ(self, state1, action, state2, reward):
-        oldQ = self.Q.get((state1, action), None)
-        if oldQ is None:
+        old_Q = self.Q.get((state1, action), None)
+        if old_Q is None:
             self.Q[(state1, action)] = reward
-        # update Q-value
         else:
-            next_maxQ = max([self.getQ(state2, act) for act in self.actions])
-            self.Q[(state1, action)] = (1 - self.alpha) * oldQ + self.alpha * (reward + self.gamma * next_maxQ)
+            next_max_Q = max([self.getQ(state2, act) for act in self.actions])
+            self.Q[(state1, action)] = (1 - self.alpha) * old_Q + self.alpha * (reward + self.gamma * next_max_Q)
     
-    # Exploration rate decay
+    # Exploration rate decay, applied after each episode
     def epsilonDecay(self, episode):
         self.epsilon = self.min_epsilon + (1 - self.min_epsilon) * np.exp(-self.epsilon_decay * episode)
-
