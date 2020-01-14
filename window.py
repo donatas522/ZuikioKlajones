72a6622e0838cb120d97a820e8a5007dcd1897ef
SilkyRock <59670053+SilkyRock@users.noreply.github.com>
2020-01-14 17:33:15 +0200

    Update window.py

--- window.py
+++ window.py
@@ -1,50 +1,55 @@
+
 import pygame
 import config as cfg
 import sys
 import time
 
+
 class Window:
     activated = False
     paused = False
     title = ''
-    updateEvery = 1
-    delay = 1
+    update_every = 1
+    delay = 10
+    size = 50
     clock = pygame.time.Clock()
     
     def __init__(self):
         self.world = None
-        
     
-    def activate(self, size=50):
+    
+    def activate(self, size=size):
         self.size = size
         pygame.init()
         w = (cfg.cols+2) * size
         h = (cfg.rows+2) * size
         self.screen =  pygame.display.set_mode((w, h))
         self.activated = True
-        self.defaultColour = pygame.Color(cfg.background_color)
+        self.default_color = pygame.Color(cfg.background_color)
         self.redraw()
-        
+    
+    
     def redraw(self):
-        self.screen.fill(self.defaultColour)
+        self.screen.fill(self.default_color)
         for row in self.world.grid:
             for cell in row:
+                if cell.wall:
+                    c = pygame.Color(cfg.wall_color)
+                
                 if len(cell.agents) > 0:
                     c = self.getColour(cell.agents[0])
                 else:
                     c = self.getColour(cell)
                 
-                if cell.wall:
-                    c = pygame.Color(cfg.wall_color)
-                
-                if c != self.defaultColour:
-                    try:                      
+                if c != self.default_color:
+                    try:
                         self.screen.fill(c, (cell.x*self.size, cell.y*self.size, self.size, self.size))
                     except:
-                        print('Error: invalid color:', c)
+                        print('ERROR: invalid color:', c)
                 rect = pygame.Rect(cell.x*self.size, cell.y*self.size, self.size, self.size)
                 pygame.draw.rect(self.screen, pygame.Color(cfg.wall_color), rect, 2)
-        
+    
+    
     def getColour(self, obj):
         c = getattr(obj, 'colour', None)
         if c is None:
@@ -57,10 +62,11 @@ class Window:
             return c
         return pygame.Color(c)
     
+    
     def update(self):
         if not self.activated:
             return
-        if self.world.age % self.updateEvery != 0 and not self.paused:
+        if self.world.age % self.update_every != 0 and not self.paused:
             return
         self.setTitle(self.title)
         for event in pygame.event.get():
@@ -83,12 +89,14 @@ class Window:
         if self.delay > 0:
             time.sleep(self.delay * 0.1)
         #self.clock.tick(60)
-            
+    
+    
     def pause(self, event=None):
         self.paused = not self.paused
         while self.paused:
             self.update()
-            
+    
+    
     def setTitle(self, title):
         if not self.activated:
             return
@@ -97,17 +105,18 @@ class Window:
         if pygame.display.get_caption()[0] != title:
             pygame.display.set_caption(title)
     
+    
     def makeTitle(self, world):
         text = 'age: %d' % world.age
         extra = []
         if world.rabbit_win:
-            extra.append('energy=%d' % world.rabbit_win)
+            extra.append('energy=%d' % world.rabbit_win) # cia turi buti energy, bet tada kazkaip reikia nurodyt Rabbit instance
         if world.wolf_win:
             extra.append('wolf_win=%d' % world.wolf_win)
         if self.paused:
             extra.append('paused')
-        if self.updateEvery != 1:
-            extra.append('skip=%d' % self.updateEvery)
+        if self.update_every != 1:
+            extra.append('skip=%d' % self.update_every)
         if self.delay > 0:
             extra.append('delay=%d' % self.delay)
 
